382b638bb8584b0ba927fbdfacbbd9a196ef2bd4
Jun Kato <i@junkato.jp>
2020-03-14 14:48:30 +0900

    Next.JS + MDX + theme-ui starts working properly

---
+++ theme.js
@@ -0,0 +1,23 @@
+// theme ui
+export default {
+  fonts: {
+    body: "system-ui, sans-serif",
+    heading: '"Avenir Next", sans-serif',
+    monospace: "Menlo, monospace"
+  },
+  colors: {
+    text: "#000",
+    background: "#fff",
+    primary: "#c00"
+  },
+  styles: {
+    h1: {
+      fontSize: 32,
+      fontFamily: "heading",
+      fontWeight: "heading",
+      color: "primary",
+      mt: 4,
+      mb: 2
+    }
+  }
+};
